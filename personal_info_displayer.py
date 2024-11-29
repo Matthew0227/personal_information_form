67f2c842a788b5de26f8b6545189dc383875ac0d
Matthew0227 <matthewjaveniar2@gmail.com>
2024-11-29 23:52:33 +0800

    finishing the code

---
+++ personal_info_displayer.py
@@ -0,0 +1,47 @@
+def letters_only_input(prompt):
+    while True:
+        name_ = input(prompt)
+        if any(char.isdigit() for char in name_):
+            print("Please enter letter only")
+        else:
+            return name_
+
+
+while True:
+    print("-------------------")
+    print("Data Information")
+    print("-------------------")
+    print("Enter the following to retrieve your information")
+    surname = letters_only_input("Please enter your last name: ")
+    first_name = letters_only_input("Please enter your first name: ")
+
+    while True:
+            age = input("Age: ")
+            if age.isdigit():
+                age = int(age)
+                if 0 < age < 121:
+                    age = str(age)
+                    break
+            else:
+                print("Please enter your age (1-120 only).")
+
+    file_data = f"./{surname}_{first_name}_{age}.txt"
+
+    try:
+        with open(file_data, "r") as file_txt:
+            print(file_txt.read())
+    except FileNotFoundError:
+        print("The person does not exist in the data, would you like to try again?: ")
+
+
+    try:
+        retry = input("add another person?(y/n): ")
+        if retry == "y" or "n":
+            print()
+    except:
+        print("enter y/n: ")
+
+    if retry == "y":
+        print()
+    elif retry == "n":
+        break
--- personal_info_form.py
+++ personal_info_form.py
@@ -1,3 +1,5 @@
+personal_informations_datas = {}
+
 def letters_only_input(prompt,error):
     while True:
         name_ = input(prompt)
@@ -11,7 +13,7 @@ def phone_numbers_input(prompt1,error1):
         phone_num = input(prompt1)
         if len(phone_num) == 11:
             if phone_num.isdigit():
-                phone_num = int(phone_num)
+                phone_num = str(phone_num)
                 return phone_num
         else:
             print(error1)
@@ -26,6 +28,7 @@ while True:
         if age.isdigit():
             age = int(age)
             if 0 < age < 121:
+                age = str(age)
                 break
         else:
             print("Please enter your age (1-120 only).")
@@ -46,3 +49,71 @@ while True:
     telephone_number = phone_numbers_input("Telephone number: ", "Please enter your telephone number (11 digits only).")
     phone_number = phone_numbers_input("Phone number: ", "Please enter your phone number (11 digits only).")
     gmail = input("Gmail: ")
+
+
+    personal_informations_datas[surname+"_"+first_name+"_"+age] = {
+        "First name" : first_name,
+        "Middle name" : middle_name,
+        "Surname name" : surname,
+        "Age" : age,
+        "Birthdate" : birthdate,
+        "Nationality" : nationality,
+        "Occupation" : occupation,
+        "Address" : address,
+        "Telephone number" : telephone_number,
+        "Phone number" : phone_number,
+        "Gmail" : gmail,
+    }
+
+    with open(f"./{surname}_{first_name}_{age}.txt", "w") as file_txt:
+        file_txt.write("-------------------\n")
+        file_txt.write("Personal Data Form\n")
+        file_txt.write("-------------------\n")
+        file_txt.write("First name: ")
+        file_txt.write(first_name)
+        file_txt.write("\n")
+        file_txt.write("Middle name: ")
+        file_txt.write(middle_name)
+        file_txt.write("\n")
+        file_txt.write("Last name: ")
+        file_txt.write(surname)
+        file_txt.write("\n")
+        file_txt.write("-------------------\n")
+        file_txt.write("Age: ")
+        file_txt.write(age)
+        file_txt.write("\n")
+        file_txt.write("Birthdate: ")
+        file_txt.write(birthdate)
+        file_txt.write("\n")
+        file_txt.write("Nationality: ")
+        file_txt.write(nationality)
+        file_txt.write("\n")
+        file_txt.write("Occupation: ")
+        file_txt.write(occupation)
+        file_txt.write("\n")
+        file_txt.write("Address: ")
+        file_txt.write(address)
+        file_txt.write("\n")
+        file_txt.write("Telephone number: ")
+        file_txt.write(telephone_number)
+        file_txt.write("\n")
+        file_txt.write("Phone number: ")
+        file_txt.write(phone_number)
+        file_txt.write("\n")
+        file_txt.write("Gmail: ")
+        file_txt.write(gmail)
+        file_txt.write("\n")
+
+
+        
+    try:
+        retry = input("add another person?(y/n): ")
+        if retry == "y" or "n":
+            print()
+    except:
+        print("enter y/n: ")
+
+    if retry == "y":
+        print()
+    elif retry == "n":
+        break
